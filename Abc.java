478b044cc6734eeee0e1cd2f59946e3b72ea360d
Sheetal Sreedhar <sheetal.sreedhar@ibm.com>
2022-06-06 11:08:09 +0530

    Added for loop

--- Abc.java
+++ Abc.java
@@ -2,6 +2,9 @@ public class Abc
 {
 	public static void main(String args[])
 	{
-		System.out.println("Hello Everybody");
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println("Just for Demo");
+		}
 	}
 }
